9e3b14de60b735f55ec31ff9f52e2bfcdef7dbac
AdrianoEusebio <adrianoeusebio2006c1@gmail.com>
2025-05-07 01:01:05 -0400

    ajeitei

--- Api/api_app/models.py
+++ Api/api_app/models.py
@@ -1,3 +1,17 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class GiftCard(models.Model):
+    empresa = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    codigo = models.CharField(max_length=12, blank=True, null=True)
+    descricao = models.TextField(blank=True)
+    isBuy = models.BooleanField(default=False)
+
+    def save(self, *args, **kwargs):
+        if self.isBuy and not self.codigo:
+            self.codigo = self.gerar_codigo()
+        super().save(*args, **kwargs)
+
+    def gerar_codigo(self):
+        return str(uuid.uuid4()).replace("-", "")[:12].upper()
