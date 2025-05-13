5bb24cd80a57196043e204c2296ac795fff2bd69
AdrianoEusebio <adrianoeusebio2006c1@gmail.com>
2025-05-12 21:09:21 -0400

    modificações no view.py e na url.py

--- Api/api_app/views.py
+++ Api/api_app/views.py
@@ -1,10 +1,1 @@
 # api_app/views.py
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-
-class HelloView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        return Response({"message": f"Olá, {request.user.username}!"})
