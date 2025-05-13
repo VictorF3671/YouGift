3c95e30df6d10f51ff4fa50df7ba1a5e49d9bacd
AdrianoEusebio <adrianoeusebio2006c1@gmail.com>
2025-05-12 22:22:47 -0400

    adicionando o cors-headers

--- Api/api_app/models.py
+++ Api/api_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class GiftCard(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=255)
+    image = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
+    
+class giftCard_serializer(models.Model):
+    giftcard = models.ForeignKey(GiftCard, on_delete=models.CASCADE)
+    code = models.CharField(max_length=100)
+    is_used = models.BooleanField(default=False)
+    in_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.giftcard.name
