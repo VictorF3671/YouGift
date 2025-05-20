5d466fedaab8871bb4a7d8b16d474ce51a4a5d2e
AdrianoEusebio <adrianoeusebio2006c1@gmail.com>
2025-05-19 22:55:28 -0400

    style: stilização no GiftCardValueSerializer para aparecer o nome do giftCard ao inves do ID

--- Api/api_app/serializer/giftCard_value_serializer.py
+++ Api/api_app/serializer/giftCard_value_serializer.py
@@ -1,8 +1,10 @@
 from ..models import GiftCardValue
 from rest_framework import serializers
+from ..serializer import GiftCardSerializer
 
 class GiftCardValueSerializer(serializers.ModelSerializer):
+    gift_card = GiftCardSerializer(read_only=True)
     
     class Meta:
         model = GiftCardValue
-        fields = '__all__' 
+        fields = ['gift_card','value','created_at']
