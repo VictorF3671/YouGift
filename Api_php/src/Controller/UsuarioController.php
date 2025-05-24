cd026a54a627c04b4dda0944b437671893a08f4e
AdrianoEusebio <adrianoeusebio2006c1@gmail.com>
2025-05-24 01:21:19 -0400

    feat: add o endpoint de Vizualizar

--- Api_php/src/Controller/UsuarioController.php
+++ Api_php/src/Controller/UsuarioController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Core\UseCase\Usuario\RegistrarUsuario;
+use Core\UseCase\Usuario\VisualizarUsuario;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,7 +13,8 @@ use Symfony\Component\Routing\Annotation\Route;
 class UsuarioController extends AbstractController
 {
     public function __construct(
-        private RegistrarUsuario $registrarUsuario
+        private RegistrarUsuario $registrarUsuario,
+        private VisualizarUsuario $visualizarUsuario
     ) {}
 
     #[Route('/registrar-usuario', methods: ['POST'])]
@@ -37,5 +39,21 @@ class UsuarioController extends AbstractController
                 'erro' => 'Erro inesperado: ' . $e->getMessage()
             ], JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
         }
+    }
+
+    #[Route('/visualizar/{id}', name: 'visualizar_usuario', methods: ['GET'])]
+    public function viewUser(string $id): JsonResponse
+    {
+        $id = (int)$id;
+        $usuario = $this->visualizarUsuario->executar($id);
+
+        return new JsonResponse([
+            'id' => $usuario->getId(),
+            'nome' => $usuario->getNome(),
+            'email' => $usuario->getEmail(),
+            'cpf' => $usuario->getCpf(),
+            'grupo' => $usuario->getTipo()->toString(),
+            'criado_em' => $usuario->getCriadoEm()->format('Y-m-d H:i:s'),
+        ]);
     }
 }
