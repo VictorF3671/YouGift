d988e3f5c602460614034a7d34c95feb2a35b80d
Vicor Noronha <victorhugonoronhas@gmail.com>
2025-05-26 23:02:25 -0400

    feat<build em um android> : buildando em android, funcionando apenas com live reload

--- App/YouGift/capacitor.config.ts
+++ App/YouGift/capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'YouGift',
-  webDir: 'www'
+  webDir: 'www',
+  server: {
+    androidScheme: 'https' 
+  }
 };
 
 export default config;
